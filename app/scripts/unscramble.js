0e5d741719d688c38e6a173df9a6a6b10fa61e62
HathawayC <c940113@qq.com>
2015-09-07 17:06:34 +0800

    一个uncramble页面放多条题目

--- app/scripts/unscramble.js
+++ app/scripts/unscramble.js
@@ -1,21 +1,37 @@
 $(function(){
+  initial();
+  addPlay();
+  nextClick();
+});
 
+// 初始化
+function initial(){
+  $('.part ').css('display','none');
+  $('.part:first').css('display','block');
+}
+
+// 播放MP3文件
+function addPlay(){
+
+  //0表示未播放状态，1表示播放状态
   var play = 0;
-  $('.horn').click(function(){
+
+  $('.text').click(function(){
     if(play == 0){
-      soundPlaying($(this));
       play = 1;
+      soundPlaying($(this).find('img'));
     }
-    else{
-      soundStop($(this));
-      play = 0;
-    }
+    //播放录音中间，停止播放
+    // else{
+    //   stopSound($(this).find('img'));
+    //   play = 0;
+    // }
   });
 
-  console.log(audios[0].text);
 
   function soundPlaying(ob){
     ob.attr('src','../imgs/icon_sound_press.png');
+
     soundManager.setup({
       onready : function(){
         var mysound = soundManager.createSound({
@@ -33,14 +49,33 @@ $(function(){
           to: 670,                   // 安装服务器返回的结束时间
           onstop: function() {
             soundManager._writeDebug('sound stopped at position ' + this.position);
-            soundStop(ob);
+            play=0;                    //把mp3播放状态改回为停止
+            stopSound(ob);             //把喇叭改为未播放状态
             // note that the "to" target may be over-shot by 200+ msec, depending on polling and other factors.
           }
-        })
+        });
       }
-    })
+    });
   }
-  function soundStop(ob){
+  function stopSound(ob){
     ob.attr('src','../imgs/icon_sound.png');
   }
-});
+}
+function nextClick(){
+  var length = $('.part').length;
+  var index = 0;
+  $('#next').click(function(){
+
+    //*****************************************************
+    //******************当学习完了这单元该怎么办***********
+    //****************************************************** 
+
+    if(index == $('.part').length - 1)
+      alert('Congratulations!\n You have finish this unit.');
+    else{
+      index++;
+      $('.part').css('display','none');
+      $('.part').eq(index).css('display','block');
+    }
+  });
+}
