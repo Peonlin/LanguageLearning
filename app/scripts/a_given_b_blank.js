3a0ec8c8edc5019760a1b3d430167ac5487d04f0
Peonlin <peonpp@163.com>
2015-09-29 21:57:27 +0800

    a_given_b_blank前端实现

--- app/scripts/a_given_b_blank.js
+++ app/scripts/a_given_b_blank.js
@@ -1,6 +1,52 @@
 ;$(document).ready(function() {
   initial(); 
+  addEvents();
 });
 function initial(){
+  $('.part').eq(0).addClass('active');
   $('.part:gt(0)').hide();
+};
+function addEvents(){
+  $('.horn').click(function(){
+    if($(this).hasClass('press')){
+      $(this).removeClass('press');
+      $(this).attr('src','../imgs/icon_sound.png'); 
+    }
+    else{
+      $(this).addClass('press');
+      $(this).attr('src','../imgs/icon_sound_press.png'); 
+    }
+  });
+  $('.next').click(function(){
+    $('.part').removeClass('active');
+    $(this).parent().addClass('active');
+    if($('.active').find('input[type=text]').val().length == 0)
+      $('.active').find('.inputTip').css('display','block');
+    else{
+      if($(this).text() =='Answer'){
+        showAnwser();
+        $(this).text('Next');
+      }
+      else{
+        showNextQestion();
+        $(this).text('Answer');
+      }
+    }
+  });
+};
+
+function showAnwser(){
+  $('.active').find('.answers').show();
 }
+function showNextQestion(){
+  var ob = $('.active');
+  ob.hide();
+  ob.next().show();
+  if($('.part:last').hasClass('active')){
+    $('.finish').show();
+  }
+  else{
+    var num = parseInt($('.toLearn span').eq(0).text()) +1;
+    $('.toLearn span').text(num + '');
+  }
+};
