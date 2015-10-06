a394014bbe6cde0c0f5af98382a498a89c1b59fd
HathawayC <c940113@qq.com>
2015-10-07 03:43:26 +0800

    初步完成a_cloze_b_given 、a_given_b_cloze 、multiple_choice页面

--- app/scripts/matchupclick.js
+++ app/scripts/matchupclick.js
@@ -8,7 +8,6 @@ function initial(){
   $('.part ').hide();
   $('.part:first').show();
   addActive($('.part:first'));
-  showInputs();
   addFocus($('.part:first').find('input').eq(0));
   $('.plain').click(function(){
     if($(this).parent().find('.horn').hasClass('press')){
@@ -22,7 +21,6 @@ function initial(){
   });
   $('.part input').focus(function(){
     addFocus($(this));
-    showChoice();
   });
   $('.scrambled .piece').click(function(){
     $('.active input.focus').val($(this).text());
@@ -55,12 +53,7 @@ function inputIsEdited(){
   return true;
 }
 function showAnswer(){
-  var str = 'Answer: &nbsp&nbsp' + $('.active .answer').eq(0).text();
-  for(var i=1;i<$('.active .answer').length;i++){
-    str += '、 ' +$('.active .answer').eq(i).text();
-  }
-  $('.active .answer:first').html(str);
-  $('.active .answer:first').show();
+  $('.active .desired').show();
 };
 function showInputTip(){
   $('.active').find('.inputTip').show();
@@ -82,28 +75,6 @@ function showNextQestion(){
   }
 };
 
-function showInputs(){
-  for(var i=0;i<$('.part .plain').length;i++){
-    var text = $('.part .plain').eq(i).text();
-    text = text.replace(/\[h\]/g,"<input type='text'><span class='answer'>").replace(/\[-h\]/g,'</span>');
-    $('.part .plain').eq(i).html(text);
-    insertAnswer($('.part').eq(i));
-  }
-};
-function showChoice(){
-  //---------------------------------------------------------------------
-  //--------------------------还未想好怎么做-----------------------------
-  //----------------------------------------------------------------------
-};
-function insertAnswer(ob){
-  var myanswer = ob.find('.answer').text();
-  var length = ob.find('.scrambled .piece').length;
-  var index = getRandomNum(0,length)
-  if(index == length)
-    $("<span class='piece'>"+ myanswer +"</span>").insertAfter(ob.find('.scrambled .piece').eq(index-1));
-  else
-  $("<span class='piece'>"+ myanswer +"</span>").insertBefore(ob.find('.scrambled .piece').eq(index));
-}
 function addActive(ob){
   $('.part').removeClass('active');
   ob.addClass('active');
@@ -111,9 +82,4 @@ function addActive(ob){
 function addFocus(ob){
   $('.part input').removeClass('focus');
   ob.addClass('focus');
-};
-function getRandomNum(Min,Max){
-  var Range = Max - Min;
-  var Rand = Math.random();
-  return(Min + Math.round(Rand * Range));
 };
