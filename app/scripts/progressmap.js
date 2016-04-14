27b24d62c655fe039c5588c83a74a97adfa5b6b2
HathawayC <c940113@qq.com>
2016-04-14 15:24:21 +0800

    a given b cloze 下一个set

--- app/scripts/progressmap.js
+++ app/scripts/progressmap.js
@@ -1,17 +1,24 @@
 $(document).ready(function() {
     // 初始化数据
-    for(var i=0;i<19;i++){
-    	var ob = $('#right .right').eq(0).clone(true);
-    	ob.insertAfter($('#right .right:last'));
+    // tour 和 unit是从1开始计数，而不是从0开始的。
+    var tour = 5;
+    var unit = 14;
+    for(var i=1;i<=18;i++){
+    	var ob = $('#right').append('<div class="right"></div>');
+    	for(var j =1 ;j<=20;j++){
+    		if(i<=tour && j<=unit)
+	    		$('#right .right').eq(i-1).append('<a href = begin_lesson?tour='+ i +'&unit='+ j +' class = "rightli learned">Unit '+ i+'_'+j +'</a>');
+	    	else
+	    		$('#right .right').eq(i-1).append('<a class = "rightli tolearn">Unit '+ i+'_'+j +'</a>');
+    	}
     }
-    var tour = 2;
-    var unit = 5;
+    // 因为数据是从1开始计数，减1变成从0开始计数
+    tour -- ;
+    unit --;
     $('#left .leftli:lt('+tour+')').addClass('learned');
     $('#left .leftli:eq(' + tour +')').addClass('learned active');
     $('#left .leftli:gt('+tour+')').addClass('tolearn');
     $('#right .right').eq(tour).addClass('active');
-    $('#right .right:lt( '+tour+' ) .rightli').removeClass('tolearn').addClass('learned');
-    $('#right .right.active .rightli:lt('+unit+')').removeClass('tolearn').addClass('learned');
     $('#right .right.active .rightli:eq(' + unit +')').addClass('learned active');
 
     // 添加事件
